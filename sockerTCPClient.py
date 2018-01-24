3ecc9e00149f3413218d806f23adbfa227b7a941
guangningsun <sun-guangning@126.com>
2018-01-24 01:00:25 -0500

    增加python TCP代码

---
+++ sockerTCPClient.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+
+import socket
+from SocketServer import (TCPServer as TCP, StreamRequestHandler as SRH)
+
+HOST ='127.0.0.1'
+SPORT = 21567
+DPORT = 21568
+BUFSIZ = 1024
+SADDR = (HOST, SPORT)
+DADDR = (HOST, DPORT)
+
+#class MyRequestHandler(SRH):
+#    def handle(self):
+#        print 'connected from ',self.client_address
+#        data = raw_input('response>>')
+#        self.wfile.write("[%s] %s " % (ctime(), data)
+
+
+while True:
+    tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpCliSock.connect(SADDR)
+    data = raw_input('请输入>')
+    if not data:
+        break
+    tcpCliSock.send("%s\r\n" % data)
+    resp = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print resp
+    tcpCliSock.close()
---
+++ sockerTCPServer.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+
+from SocketServer import (TCPServer as TCP, StreamRequestHandler as SRH)
+import socket
+from time import ctime
+
+HOST = '127.0.0.1'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+class MyRequestHandler(SRH):
+    def handle(self):
+        print 'connected from ',self.client_address
+        data = raw_input('>>>>>')
+        self.wfile.write("[%s] %s" % (ctime(), data))
+
+tcpServ = TCP(ADDR, MyRequestHandler)
+print 'waiting for connection...'
+tcpServ.serve_forever()
